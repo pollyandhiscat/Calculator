5db7ed27ecbb1536959a3c45719aad9e41e8fad8
pollyandhiscat <pollyandhiscat@outlook.com>
2023-09-14 21:00:45 -0700

    Create function which will activate (add event listeners) on each button

---
+++ js/main.js
@@ -0,0 +1,42 @@
+function activateButtons {
+
+    /* Adds event listeners to each button
+    on the calculator. This allows the buttons
+    to send their value when they are clicked.
+    */
+
+    const button_1 = document.querySelector('#button-1');
+
+    const button_2 = document.querySelector('#button-2');
+
+    const button_3 = document.querySelector('#button-3');
+
+    const button_4 = document.querySelector('#button-4');
+
+    const button_5 = document.querySelector('#button-5');
+
+    const button_6 = document.querySelector('#button-6');
+
+    const button_7 = document.querySelector('#button-7');
+
+    const button_8 = document.querySelector('#button-8');
+
+    const button_9 = document.querySelector('#button-9');
+
+    const button_0 = document.querySelector('#button-0');
+
+    const button_equals = document.querySelector('#button-=');
+
+    const button_add = document.querySelector('#button-+');
+
+    const button_divide = document.querySelector('#button-/');
+
+    const button_multiply = document.querySelector('#button-*');
+
+    const button_clear = document.querySelector('#button-clear');
+
+    const button_enter = document.querySelector('#button-enter');
+
+
+
+}
