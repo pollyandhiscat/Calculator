320cac371335eceb88969a8cc6b8987d0893ce73
pollyandhiscat <pollyandhiscat@outlook.com>
2023-09-15 20:20:34 -0700

    Add event listener for all calculator buttons

--- js/main.js
+++ js/main.js
@@ -11,10 +11,9 @@ function addValueToScreen(value) {
 
 function clearScreen() {
 
-    const display = document.querySelector('#display');
+    const screen_text = document.getElementById('#screen-text');
     screenContent = [];
-    let content = screenContent.join();
-    display.textContent = content
+    screen_text.textContent = '';
 
 }
 
@@ -26,33 +25,74 @@ function activateButtons() {
     */
 
     const button_1 = document.querySelector('#button-1');
-    button_1.addEventListener('click', addValueToScreen(button_1.textContent));
+    button_1.addEventListener('click', () =>{
+        
+        addValueToScreen(button_1.textContent)});
     
     const button_2 = document.querySelector('#button-2');
+    button_2.addEventListener('click', () =>{
+        
+        addValueToScreen(button_2.textContent)});
 
     const button_3 = document.querySelector('#button-3');
+    button_3.addEventListener('click', () =>{
+        
+        addValueToScreen(button_3.textContent)});
 
     const button_4 = document.querySelector('#button-4');
+    button_4.addEventListener('click', () =>{
+        
+        addValueToScreen(button_4.textContent)});
 
     const button_5 = document.querySelector('#button-5');
+    button_5.addEventListener('click', () =>{
+        
+        addValueToScreen(button_5.textContent)});
 
     const button_6 = document.querySelector('#button-6');
+    button_6.addEventListener('click', () =>{
+        
+        addValueToScreen(button_6.textContent)});
 
     const button_7 = document.querySelector('#button-7');
+    button_7.addEventListener('click', () =>{
+        
+        addValueToScreen(button_7.textContent)});
 
     const button_8 = document.querySelector('#button-8');
+    button_8.addEventListener('click', () =>{
+        
+        addValueToScreen(button_8.textContent)});
 
     const button_9 = document.querySelector('#button-9');
+    button_9.addEventListener('click', () =>{
+        
+        addValueToScreen(button_9.textContent)});
 
     const button_0 = document.querySelector('#button-0');
+    button_0.addEventListener('click', () =>{
+        
+        addValueToScreen(button_0.textContent)});
 
     const button_equals = document.querySelector('#button-equals');
+    button_equals.addEventListener('click', () =>{
+        
+        addValueToScreen(button_equals.textContent)});
 
     const button_add = document.querySelector('#button-add');
+    button_add.addEventListener('click', () =>{
+        
+        addValueToScreen(button_1.textContent)});
 
     const button_divide = document.querySelector('#button-divide');
+    button_divide.addEventListener('click', () =>{
+        
+        addValueToScreen(button_divide.textContent)});
 
     const button_multiply = document.querySelector('#button-multiply');
+    button_multiply.addEventListener('click', () =>{
+        
+        addValueToScreen(button_multiply.textContent)});
 
     const button_clear = document.querySelector('#button-clear');
     button_clear.addEventListener('click', () => {
